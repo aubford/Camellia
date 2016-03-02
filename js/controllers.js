28b0d014f6107bb6287d6e3262e696b65e90ac11
Aubrey Ford <musicaubrey@gmail.com>
2016-03-02 11:32:18 -0700

    >

--- js/controllers.js
+++ js/controllers.js
@@ -7,9 +7,8 @@ app.controller('MainController', ['$scope', 'cart', 'teas', 'bag', function($sco
 
       $scope.bagAdd =function(item){
         bag.update(item, true)
-        item.quantity = '1'
 
-        //!!!!Why doesn't this update automatically
+        item.quantity = '1'
         $scope.count = cart.length
       }
 
@@ -18,17 +17,37 @@ app.controller('MainController', ['$scope', 'cart', 'teas', 'bag', function($sco
 app.controller('CartController', ['$scope', "bag", "cart", function($scope, bag, cart){
     $scope.cart=cart
 
+    $scope.totalIt = function(){
+      return cart.reduce(function(sum, e){
+        return sum + e.subtotal
+      },0)
+    }
+
+    $scope.total = $scope.totalIt()
+
+
     $scope.remove= function(item){
       cart.forEach(function(e,i){
         if(e._id==item._id){
           cart.splice(i,1)
         }
       })
-    $scope.cart=cart
+    $scope.total = $scope.totalIt()
     }
     //!!!!is this best way to get cart provider data into view?
-    $scope.cartUpdate = function(item){
+    $scope.cartUpdate = function(item, that){
+      console.log(that);
+      if (item.quantity > 0){
         bag.update(item, false)
         $scope.price= cart.price
+      }else{
+        $scope.remove(item)
+      }
+      $scope.total = $scope.totalIt()
+    }
+
+    $scope.placeOrder = function(){
+      cart.splice(0,cart.length)
+      $scope.total = $scope.totalIt()
     }
 }])
--- js/services.js
+++ js/services.js
@@ -36,21 +36,20 @@ app.factory('bag', ['cart', function(cart){
             if(homePage){
               e.quantity += Number(item.quantity)
             }else{
-              e.quantity = Number(item.quantity)    
+              e.quantity = Number(item.quantity)
             }
             e.subtotal = Number(item.quantity) * Number(e.price)
           }
         })
       }else{
-        //!!!!How to make a duplicate of the object for pushing
         var pusher = Object.assign({},item)
         pusher.quantity = Number(item.quantity)
         pusher.subtotal = Number(item.quantity) * Number(item.price)
         cart.push(pusher)
-
       }
     }
   }
 }])
+
 
 app.value('cart', [])
